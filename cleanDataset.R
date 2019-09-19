1e4501365d9e124816dcfb6e95c47613dbff6234
Mandy Clemente <mjclemen@andrew.cmu.edu>
2019-09-19 18:18:03 -0400

    Update cleanDataset.R
    Removed empty columns
    Removed "\n" from last column of values
    Removed rows with null values
    Wrote updates to csv file

--- cleanDataset.R
+++ cleanDataset.R
@@ -1,6 +1,9 @@
 deaths <- read.csv("JournalistDeaths.csv", na.strings=c("","NA"))[,1:17]
 deaths
+deaths$Tortured <- as.character(deaths$Tortured)
+for (i in 1:nrow(deaths)) {
+  deaths[i,17] <- substr(deaths[i,17], 1, nchar(deaths[i,17])-1)
+}
 deaths <- na.omit(deaths)
 deaths
-deaths <- deaths[,c(2:17)]
-write.csv(deaths, file = 'JournalistDeaths.csv')
+write.csv(deaths, file = 'JournalistDeaths.csv', row.names = FALSE)
