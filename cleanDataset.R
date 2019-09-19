fde163d04d1c46fe673cc190d9f52816bac93c0b
Mandy Clemente <mjclemen@andrew.cmu.edu>
2019-09-19 16:42:42 -0400

    Update cleanDataset.R
    Write to csv and drop first column containing the record number

--- cleanDataset.R
+++ cleanDataset.R
@@ -2,4 +2,5 @@ deaths <- read.csv("JournalistDeaths.csv", na.strings=c("","NA"))[,1:17]
 deaths
 deaths <- na.omit(deaths)
 deaths
+deaths <- deaths[,c(2:17)]
 write.csv(deaths, file = 'JournalistDeaths.csv')
