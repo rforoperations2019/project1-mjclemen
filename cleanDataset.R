311806fd4c30187956fd864a99fa82c30ea23700
Mandy Clemente <mjclemen@andrew.cmu.edu>
2019-09-20 14:05:30 -0400

    Removed null rows and columns, and removed "\n" character from last column's values

--- cleanDataset.R
+++ cleanDataset.R
@@ -1,9 +1,7 @@
 deaths <- read.csv("JournalistDeaths.csv", na.strings=c("","NA"))[,1:17]
-deaths
 deaths$Tortured <- as.character(deaths$Tortured)
 for (i in 1:nrow(deaths)) {
   deaths[i,17] <- substr(deaths[i,17], 1, nchar(deaths[i,17])-1)
 }
 deaths <- na.omit(deaths)
-deaths
 write.csv(deaths, file = 'JournalistDeaths.csv', row.names = FALSE)
