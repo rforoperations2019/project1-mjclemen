e22b5f34c65a7bd091fa3d63563269ffca63b044
Mandy Clemente <mjclemen@andrew.cmu.edu>
2019-09-21 17:25:12 -0400

    Used complete case to find any "na" values left over after na.omit to remove records containing "na" values

--- cleanDataset.R
+++ cleanDataset.R
@@ -2,20 +2,16 @@ deaths <- read.csv("JournalistDeaths.csv", stringsAsFactors=F, na.strings=c("","
 deaths$Tortured <- as.character(deaths$Tortured)
 for (i in 1:nrow(deaths)) {
   deaths[i,17] <- gsub(x = deaths[i, 17], pattern = "\n", replacement = "")
-    #substr(deaths[i,17], 1, nchar(deaths[i,17])-1)
 }
 deaths <- na.omit(deaths)
-deaths
+deaths[complete.cases(deaths),]
 names(deaths) <- gsub(x = names(deaths), pattern = "\\.", replacement = " ")
-nrow(deaths)
 for (i in 1:nrow(deaths)) {
   for (j in 1:ncol(deaths)) {
-    na_value <- sum(deaths[i,j] == "na", na.rm = TRUE)
+    na_value <- sum(str_trim(deaths[i,j]) == "na", na.rm = TRUE)
     if (na_value == 1) {
       deaths <- deaths[-i,]
     }
   }
 }
-nrow(deaths)
-deaths
 write.csv(deaths, file = 'JournalistDeaths.csv', row.names = FALSE)
