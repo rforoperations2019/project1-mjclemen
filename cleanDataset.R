43e0d817dac650d722f1d324c878ede4f3232980
Mandy Clemente <mjclemen@andrew.cmu.edu>
2019-09-21 16:12:10 -0400

    Working on deleting rows containing "na" string

--- cleanDataset.R
+++ cleanDataset.R
@@ -1,8 +1,16 @@
-deaths <- read.csv("JournalistDeaths.csv", na.strings=c("","NA"))[,1:17]
+deaths <- read.csv("JournalistDeaths.csv", stringsAsFactors=F, na.strings=c("","NA"))[,1:17]
 deaths$Tortured <- as.character(deaths$Tortured)
 for (i in 1:nrow(deaths)) {
-  deaths[i,17] <- substr(deaths[i,17], 1, nchar(deaths[i,17])-1)
+  deaths[i,17] <- gsub(x = deaths[i, 17], pattern = "\n", replacement = "")
+    #substr(deaths[i,17], 1, nchar(deaths[i,17])-1)
 }
 deaths <- na.omit(deaths)
 names(deaths) <- gsub(x = names(deaths), pattern = "\\.", replacement = " ")
+deaths
+for (i in 1:nrow(deaths)) {
+  for (j in 1:ncol(deaths)) {
+    deaths <- deaths[ grep("na", deaths[i,j], invert = TRUE) , ]
+  }
+}
+deaths
 write.csv(deaths, file = 'JournalistDeaths.csv', row.names = FALSE)
